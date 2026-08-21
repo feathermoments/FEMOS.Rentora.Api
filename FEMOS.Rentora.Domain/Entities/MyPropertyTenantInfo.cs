7c3f1ed9605c20a0701913c0ac6f83434d7693c2
Shailender Kumar Rathore <62134774+ShailenderKumarRathore@users.noreply.github.com>
2026-08-21 20:30:25 +0530

    minor changes

--- FEMOS.Rentora.Domain/Entities/MyPropertyTenantInfo.cs
+++ FEMOS.Rentora.Domain/Entities/MyPropertyTenantInfo.cs
@@ -11,6 +11,7 @@ namespace FEMOS.Rentora.Domain.Entities
         public long TenantId { get; set; }
         public long PropertyId { get; set; }
         public long UnitId { get; set; }
+        public long RentAgreementId { get; set; }
         public long TenantAssignmentId { get; set; }
         public string UnitNumber { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
